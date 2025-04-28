98547c945398a019a8fd1b5b0f2a30fa781d89e8
Dustin Lee <dustidl1@uci.edu>
2025-04-28 13:02:17 -0700

    boom

--- scraper.py
+++ scraper.py
@@ -1,38 +1,35 @@
 import re
-from urllib.parse import urlparse, urljoin
 import time
-from bs4 import BeautifulSoup
-import shelve
-
 import io
 import PyPDF2
 import PyPDF2.errors
 
-from utils import get_logger, normalize
-from scraper_utils.fingerprint import get_fp
-from scraper_utils.similarity import is_similar_to_visited
-from scraper_utils.tokenizer import tokenize
-from scraper_utils.answers import track_longest_page, track_subdomains, track_unique_urls, update_top50_words
+import summary
+from robots import *
+import simhash
 
-TIMEOUT_LIMIT = 60 * 5
-start_time = time.time()
+from bs4 import BeautifulSoup
+from utils import get_logger, normalize
+from urllib.parse import urljoin, urlparse
 
 scrap_logger = get_logger("SCRAPPER")
-visited_urls = set()
-visited_sites_fingerprint = set()
-THRESHOLD = 0.8
-unique_count = 0
-longest_page_url = None
-longest_page_word_count = 0
-top50words = {}
-subdomain_count = {}
-top50words = {}
+
+# Store simhashes of previously visted pages to avoid scraping duplicate content
+# For less than 1 million simhashes storing simhashes in memory is preferred
+# Time complexity of python set in operation is O(1). 
+# For 20,000 SimHashes, the estimated memory usage is: 20,000 × 8 bytes = 160,000 bytes = 160 KB
+visited_content_simhashes = set()
+
+visited_sitemaps = set()
+
+# response content size limit (bytes)
+RESP_SIZE_THRESHOLD = 500000 # (500 kb)
 
 def scraper(url, resp):
-    global longest_page_word_count
 
+    # Check that the response status is ok and that the raw response has content
     if resp.status != 200 or resp.raw_response is None:
-        if resp.status >= 300 and resp.status < 400:
+        if resp.status >= 300 and resp.status < 400:  # HTTP 3xx Redirection
             redirect_url = resp.raw_response.headers.get("Location")
 
             scrap_logger.warning(f"Status {resp.status}: Redirecting {url} -> {redirect_url}")
@@ -41,6 +38,7 @@ def scraper(url, resp):
             scrap_logger.warning(f"Skipping URL {url}: Invalid response or status {resp.status}")
             return []
 
+    # Check header fields for indication of common problematic responses 
     if is_pdf_resp(url, resp):
         scrap_logger.warning(f"Skipping {url}: pdf file")
         return []
@@ -49,13 +47,12 @@ def scraper(url, resp):
         scrap_logger.warning(f"Skipping {url}: zip file")
         return []
 
+    # if is_large_resp(url, resp, RESP_SIZE_THRESHOLD):
+    #     scrap_logger.warning(f"Skipping {url}: Content length greater than {RESP_SIZE_THRESHOLD} bytes")
+    #     return []
+
     if is_attachment_resp(url, resp):
         scrap_logger.warning(f"Skipping {url}: downloads attachment")
-        return []
-
-    # Add the is_large_resp check here
-    if is_large_resp(url, resp, threshold=10 * 1024 * 1024):  # 10 MB threshold
-        scrap_logger.warning(f"Skipping {url}: response too large")
         return []
     
     # parse as html document
@@ -71,34 +68,31 @@ def scraper(url, resp):
         text = soup.get_text(separator=" ", strip=True)
     except Exception as e:
         scrap_logger.fatal(f"Error parsing {url}: {e}")
-    
-    track_unique_urls(url, visited_urls)
-    track_subdomains(url, subdomain_count)
-    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-    text = soup.get_text()
-    track_longest_page(url, text)
-    update_top50_words(text, top50words)
 
-    ''' FINGERPRINT CODE STARTS HERE '''
-    #create fingerprint
-    #decode content to string because its a byte
-    page_content = resp.raw_response.content.decode('utf-8', errors='ignore')
-    fingerprint = tuple(get_fp(page_content))
+    # Create a list of tokens(words) in the html text
+    page_tokens = simhash.tokenize(text)
 
-    # then check if the curr page is a near dupe of ANY prev page
-        #if it is a dupe, then we dont add it, so SKIP scrawling the page
-    if is_similar_to_visited(fingerprint, visited_sites_fingerprint, THRESHOLD):
-        print(f"Skipping duplicate page: {url}")
-        return [] # So that we skip crawling when its a dupe/near-dupe
-    
-    # If it is not a dupe then add it to the visted set
-    visited_sites_fingerprint.add(fingerprint)
-    ''' FINGERPRINT CODE ENDS HERE '''
-    
-    # We extract the links, all of them from the page
+    # Update summary statistics
+    summary.update_token_frequency("summary.shelve",page_tokens)
+    summary.update_page_lengths("summary.shelve", url, page_tokens)
+
+
+    # Check for near and exact duplicate content (Simhash); Simhash also covers exact duplicate which has dist == 0
+    current_page_hash = simhash.compute_simhash(page_tokens)
+    for visited_page_hash in visited_content_simhashes:
+        dist = simhash.calculate_hash_distance(current_page_hash, visited_page_hash)
+        if dist == 0:  # Exact-duplicate
+            scrap_logger.warning(f"Skipping URL {url}: Exact Duplicate Content Match with Dist={dist}")
+            return []
+        elif dist < simhash.THRESHOLD:  # Near-duplicate
+            scrap_logger.warning(f"Skipping URL {url}: Near Duplicate Content Match with Dist={dist}")
+            return []
+    visited_content_simhashes.add(current_page_hash)
+
+    # Extract links with another soup
     links = extract_next_links(url, resp)
-    # Then just return the links that need to be crawled
-    # return [link for link in links if is_valid(link)]
+    
+    # Filter out duplicate and invalid urls (message log if needed)
     unique_links = set()
     for link in links:
         if not link:
@@ -112,8 +106,6 @@ def scraper(url, resp):
 
     return list(unique_links)
 
-''' IMPLEMENT THIS PART => scraper is important for the worker class'''
-
 def extract_next_links(url, resp):
     # Implementation required.
     # url: the URL that was used to get the page
@@ -123,29 +115,9 @@ def extract_next_links(url, resp):
     # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-
-    # simple check to see if the status code is OK => if it is NOT 200 (ok) then we need to say why, so we call out reps.error
-    if resp.status != 200:
-        print(f'Error code: {resp.error}')
-        return []
-    
-    # BeautifulSoup will turn the web page into html content so i can find a specific element by tag and more...
-    # resp.raw_response.content: the content of the page!
-
-    # use the html.parse of beautifulsoup
-    # print(f"response: {resp.raw_response.url}") 
-    # print(f"response: {resp.raw_response.content}") 
-
-    # i will store all the links in a list
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content   
     links = []
-    '''    
-    legacy version 1:
-    for hyperlink in soup.find_all('a', href=True): # i want to find all a tags where href has something
-        link = hyperlink['href']
-        print(f"Found link: {link}") 
-        links.append(link)
-    '''
+
     try:
         soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
         for anchor in soup.find_all('a', href=True):
@@ -155,129 +127,219 @@ def extract_next_links(url, resp):
             abs_url = urljoin(url, link)
             parsed = urlparse(abs_url)
 
-            # remove anyhting that starts iwth # sincei  keep seeing this and it means nothing
+            # Strip queries and defragment (remove anything after '#')
             clean_url = normalize(parsed._replace(query="", fragment="").geturl())
-            
-            # gitlab and github are bad
-            if "gitlab.ics.uci.edu" in clean_url or "github.com" in clean_url:
-                scrap_logger.info(f"Skipping GitLab/GitHub URL: {clean_url}")
-                continue
 
-            if not clean_url:
-                scrap_logger.info("Skipping empty or malformed URL")
-                continue
-
-            # print(f"Found link: {clean_url}") 
             links.append(clean_url)
 
     except Exception as e:
         scrap_logger.fatal(f"Error parsing {url}: {e}")
 
-    return list(links)
+    return links
 
-def is_valid(url):
-# Decide whether to crawl this url or not. 
-# If you decide to crawl it, return True; otherwise return False.
+def is_valid(url: str) -> bool:
+    # Decide whether to crawl this url or not. 
+    # If you decide to crawl it, return True; otherwise return False.
+    # There are already some conditions that return False.
+    
+    allowed_domains = {"ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"}
+    
     try:
-        # Break the URL into parts: scheme, netloc, path, query, etc.
-        parsed = urlparse(url)
-        if parsed.scheme not in set(["http", "https"]):
+        parsed_url = urlparse(url)
+
+        # Check if url scheme is valid
+        if parsed_url.scheme not in set(["http", "https"]):
             return False
         
-        # Check if it is within the allowed domains
-        domain = parsed.netloc.lower()
-        path = parsed.path.lower()
-        query = parsed.query.lower()
-
-        # Exclude GitLab and GitHub links
-        if "gitlab.ics.uci.edu" in domain or "github.com" in domain:
+        def is_allowed_domain(domain: str) -> bool:
+            for d in allowed_domains: 
+                if domain == d or domain.endswith("." + d):
+                    return True
+            return False
+        
+        # check host is in URL is in allowed domains
+        domain = parsed_url.netloc
+        if domain and not is_allowed_domain(domain):
+            return False
+        
+        # Avoid query strings (potential duplicate content)
+        if parsed_url.query:
             return False
 
-        # Exclude calendar and event-related URLs
-        if "/events_calendar" in path or "/calendar" in path or "/events" in path or "date=" in query or "year=" in query or "month=" in query:
-            scrap_logger.info(f"Skipping calendar/event URL: {url}")
+        # Avoid infinite trap pattern
+        MAX_DEPTH = 8
+        path_segments = [segment for segment in parsed_url.path.split('/') if segment]
+        if len(path_segments) > MAX_DEPTH:
+            return False
+        
+        # Check for unique identifier segment in path 
+        # If segment is alphanumeric and beyond a cut off length 
+        # it is highly likely to be an ID and should be ignored. 
+        MAX_SEGMENT_LENGTH = 20
+        for segment in path_segments: 
+            if segment.isalnum() and len(segment) > MAX_SEGMENT_LENGTH:
+                return False 
+
+        # Filter out "archieve.ics.uci.edu" domain. 
+        # This is the machine learning archieve.
+        if "archive.ics.uci.edu" in parsed_url.netloc:
             return False
 
-        # Exclude /pix/ URLs specifically for eppstein
-        if "~eppstein" in domain and "/pix/" in path:
-            scrap_logger.info(f"Skipping /pix/ URL: {url}")
+        # Filter out calendar pages which are potentially low-information pages.
+        if "calendar" in parsed_url.path.lower() or "calendar" in parsed_url.netloc.lower():
+            return False
+        
+        # Filter out commit pages (gitlab/github) which are potentially low-information pages.
+        if "commit" in parsed_url.path.lower():
             return False
 
-        # Rule for links
-        valid_domains = [
-            "ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"
-        ]
-        today_uci_path = "/department/information_computer_sciences/"    
-        # Allow any subdomain of the 4 domains
-        if any(domain.endswith(d) for d in valid_domains):
-            return not re.match(
-                r".*\.(css|js|bmp|gif|jpe?g|ico"
-                + r"|png|tiff?|mid|mp2|mp3|mp4"
-                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-                + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-                + r"|epub|dll|cnf|tgz|sha1"
-                + r"|thmx|mso|arff|rtf|jar|csv"
-                + r"|rm|smil|wmv|swf|wma|zip|rar|gz"
-                + r"|img|java|war|sql|mpg|ff|sh|ppsx|py|apk|svg|conf|cpp|fig|cls|ipynb|bam|odp|odc|tsv|nb|bib|z|rpm|ma)$", parsed.path.lower())
+        # Check robot.txt rules to follow politeness 
+        # Do not fetch from paths we are not allowed
+        if not can_fetch(url):
+            return False
+       
+        return not re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz"
+            + r"|img|java|war|sql|mpg|ff|sh|ppsx|py|apk|svg|conf|cpp|fig|cls|ipynb|bam|odp|odc|tsv|nb|bib|z|rpm|ma)$", parsed_url.path.lower())
 
-        # Case for today.uci.edu since we only accept this specific path
-        if domain == "today.uci.edu" and path.startswith(today_uci_path):
-            return True
-
-        # True conditions weren't triggered so we exit
-        return False
-    
     except TypeError:
-        print("TypeError for ", parsed)
-        raise
+        print ("TypeError for ", parsed_url)
+        return False
 
-# for report
-def save_to_shelve(filename="scraper_results"):
+# Sitemap Helper Methods
+def get_sitemap_urls(url: str) -> list[str]: 
     """
-    Save the scraping results to a shelve database.
+    Extracts urls of sitemaps from robots.txt
     """
-    try:
-        with shelve.open(filename) as db:
-            db['unique_count'] = unique_count
-            db['longest_page_url'] = longest_page_url
-            db['longest_page_word_count'] = longest_page_word_count
-            db['top50words'] = sorted(top50words.items(), key=lambda x: x[1], reverse=True)[:50]
-            db['subdomain_count'] = subdomain_count
-        scrap_logger.info(f"Scraping results saved to {filename}.")
+
+    parser = get_robots_parser(url)
+
+    # If no parser returned, no robots.txt exists
+    if not parser:
+        return []
+
+    # Parses the sitemap parameter in 'robots' files and return the sitemap urls
+    sitemaps_urls = parser.site_maps()
+
+    # is the sitemaps list empty?
+    if sitemaps_urls: 
+        scrap_logger.info(f"Found sitemaps for {url}: {sitemaps_urls}")
+        return sitemaps_urls
+    else:
+        return []
+
+def fetch_sitemap_urls(sitemap_url: str, config: Config, logger: Logger) -> list[str]: 
+
+    time.sleep(config.time_delay)
+    logger.info(f"Downloading sitemap: {sitemap_url}")
+    resp = download(sitemap_url, config, logger)
+    visited_sitemaps.add(sitemap_url)
+
+    # If sitemap is invalid, return empty list
+    if resp.status != 200 or not resp.raw_response:
+        logger.warning(f"Failed to download sitemap: {sitemap_url}, status: {resp.status}")
+        return []
+
+    # TODO: NEEDS TESTING, the recursive downloading and adding of sitemap content might become unmanagable
+    try: 
+        tree = ET.fromstring(resp.raw_response.content)
+        urls = set()
+
+        # Gather all URLs in the sitemap
+        url_element_stack = [url_element.text.strip() for url_element in tree.findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")]
+
+        logger.info(f"Extracted URLS from sitemap {sitemap_url}: {url_element_stack}")
+
+        # Process all urls
+        while url_element_stack:
+            # Pop a url off the stack
+            url = url_element_stack.pop().strip()
+            logger.info(f"Processing site within sitemap: {url}")
+
+            # If it's another sitemap that's valid, process
+            if is_xml_doc(url) and is_valid(url):
+                # Download the sitemap
+                time.sleep(config.time_delay)
+                logger.info(f"Downloading sitemap: {url}")
+                new_resp = download(url, config, logger)
+
+                # Mark the sitemap as visited
+                visited_sitemaps.add(url)
+
+                # If valid, extend url_element_stack with the additional urls
+                if (new_resp.status != 200 or not new_resp.raw_response):
+                    logger.warning(f"Failed to download sitemap: {url}, status: {new_resp.status}")
+                else:
+                    url_element_stack.extend([elem.text.strip() for elem in ET.fromstring(new_resp.raw_response.content).findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")])
+            # If just a site, add
+            else:
+                if is_valid(url):
+                    urls.add(url)
+
+        logger.info(f"Extracted {len(urls)} URLs from {sitemap_url}")
+        return list(urls)
+
     except Exception as e:
-        scrap_logger.error(f"Error saving to shelve: {e}")
+        logger.error(f"Unexpected error parsing sitemap {sitemap_url}: {e}")
+
+    return []
+
+def seed_frontier_from_sitemap(url: str, config: Config, logger: Logger) -> list[str]:
+    sitemap_urls = get_sitemap_urls(url)
+
+    links = []
+    if sitemap_urls:
+        for sitemap in sitemap_urls:
+            # TODO: This also returns other sitemaps. We need sitemap detection for the purpose of downloading additional
+            sitemap_links = fetch_sitemap_urls(sitemap, config, logger)
+            links.extend(sitemap_links)
+
+    return links
 
 def is_pdf_resp(url, resp):
+    """
+    """
+    
     content_type = resp.raw_response.headers.get("Content-Type", "").lower()
     
-    # First, check Content-Type header
-    if "application/pdf" in content_type:
+    # Check Content-Type header
+    if "application/pdf" is content_type: 
         return True
     
+    # 
     try:
-        with io.BytesIO(resp.raw_response.content) as pdf_stream:
-            start = pdf_stream.read(5)
-            if start != b"%PDF-":
-                return False  # Definitely not a PDF
-            
-            # Reset pointer to beginning
-            pdf_stream.seek(0)
-            
+        with io.BytesIO(resp.raw_response.content) as pdf_stream: 
             reader = PyPDF2.PdfReader(pdf_stream)
             return bool(reader.pages)
-    except Exception:
+    except PyPDF2.errors.PdfReadError:
         return False
 
 def is_zip_resp(url, resp):
+    """
+    Checks that the response contains text in html format 
+    and does not contain an attachment that will try and download  
+    """
+
     content_type = resp.raw_response.headers.get("Content-Type", "").lower()
 
-    if "application/zip" == content_type: 
+    if "application/zip" is content_type: 
         return True
     
     return False
 
 def is_html_resp(url, resp):
+    """
+    Checks that the response contains text in html format 
+    and does not contain an attachment that will try and download  
+    """
+
     content_type = resp.raw_response.headers.get("Content-Type", "").lower()
 
     if content_type.startswith("text/html"):
